c80918737d0a52c633412d955400a208f4407206
Vagner <dipnoe@gmail.com>
2023-03-25 19:50:07 +0300

    add func sort_by_date

---
+++ utils/utils.py
@@ -0,0 +1,25 @@
+import json
+
+
+def json_reader():
+    """
+    Функция читает json файл с данными об операциях
+    :return: список словарей
+    """
+    with open('../operations.json', 'r', encoding='utf-8') as file:
+        return json.load(file)
+
+
+def sort_by_date(data):
+    """
+    :param data: json файл с операциями
+    :return: список выполненных операций, отсортированных по дате
+    """
+    executed_list = []
+    for operation in data:
+        if len(operation) != 0 and operation["state"] == "EXECUTED":
+            executed_list.append(operation)
+
+    executed_list.sort(key=lambda x: x.get('date'), reverse=True)
+    return executed_list
+
