31a09ecebb5e06d87f1ebe5db48045d20cf1626c
Vagner <dipnoe@gmail.com>
2023-03-26 18:03:02 +0300

    add test_output_money

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,4 @@
-from code.utils import sort_by_date, change_date_format, masking_card, output_money
+from code.utils import sort_by_date, change_date_format, masking_card, output_money, output_data
 import pytest
 
 
@@ -52,3 +52,23 @@ def test_output_money():
     assert output_money({"amount": "67314.70", "currency": {"name": "руб."}}) == "67314.70 руб."
     assert output_money({"amount": "67314.70", "currency": {"name": "USD"}}) == "67314.70 USD"
     assert output_money({"amount": "", "currency": {"name": ""}}) == " "
+
+
+def test_output_data():
+    data = {
+        "id": 594226727,
+        "state": "CANCELED",
+        "date": "2018-09-12T21:27:25.241689",
+        "operationAmount": {
+            "amount": "67314.70",
+            "currency": {
+                "name": "руб.",
+                "code": "RUB"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Visa Platinum 1246377376343588",
+        "to": "Счет 14211924144426031657"
+    }
+    result = "12.09.2018 Перевод организации\nVisa Platinum 1246 37** **** 3588 -> Счет **1657\n67314.70 руб.\n"
+    assert output_data(data) == result
