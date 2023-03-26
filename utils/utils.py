7220b6d9d99ba7e7c4d60afd0ddd0bbfaaf2b816
Vagner <dipnoe@gmail.com>
2023-03-26 13:29:49 +0300

    add func change_date_format

--- utils/utils.py
+++ utils/utils.py
@@ -1,12 +1,12 @@
 import json
 
 
-def json_reader():
+def json_reader(file_name):
     """
     Функция читает json файл с данными об операциях
     :return: список словарей
     """
-    with open('../operations.json', 'r', encoding='utf-8') as file:
+    with open(file_name, 'r', encoding='utf-8') as file:
         return json.load(file)
 
 
@@ -15,11 +15,23 @@ def sort_by_date(data):
     :param data: json файл с операциями
     :return: список выполненных операций, отсортированных по дате
     """
-    executed_list = []
-    for operation in data:
-        if "state" in operation.keys():
-            if len(operation) != 0 and operation["state"] == "EXECUTED":
-                executed_list.append(operation)
+    sorted_list = []
 
-    executed_list.sort(key=lambda x: x.get('date'), reverse=True)
-    return executed_list
+    for operation in data:
+
+        if "state" in operation.keys():
+
+            if len(operation) != 0 and operation["state"] == "EXECUTED":
+                sorted_list.append(operation)
+
+    sorted_list.sort(key=lambda x: x.get('date'), reverse=True)
+    return sorted_list
+
+
+def change_date_format(date: str):
+    """
+    :param date: строка с датой и временем
+    :return: строку с датой в нужном формате
+    """
+    date = date.split('T')[0].split('-')
+    return '.'.join(date[::-1])
